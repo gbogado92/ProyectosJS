6dd07c47eeb4d00dd872072dd230211a6a26c792
gbogado92 <62188600+gbogado92@users.noreply.github.com>
2021-04-28 17:42:50 -0300

    Primer commit

---
+++ js/app.js
@@ -0,0 +1,18 @@
+// Variable
+      const carrito = document.querySelector('#carrito');
+      const contenedorCarrito = document.querySelector('#lista-carrito tbody');
+      const vaciarCarritoBtn = document.querySelector('#vaciar-carrito');
+      const listaCursos = document.querySelector('#lista-cursos');
+
+      cargarEventListeners();
+
+      function cargarEventListeners(){
+        //   Cuando agregas un curso presionando "Agregar al carrito"
+        listaCursos.addEventListener('click', agregarCurso);
+      }
+
+    //   Funciones.
+
+      function agregarCurso() {
+          console.log('Presionando en cursos');
+      }
